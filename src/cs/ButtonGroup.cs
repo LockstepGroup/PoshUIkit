49b2ad205daf91259d2b141112f5011078862a1c
Brian Addicks <brian@addicks.us>
2016-03-30 14:55:23 -0400

    added elements property to buttongroup

--- src/cs/ButtonGroup.cs
+++ src/cs/ButtonGroup.cs
@@ -1,15 +1,22 @@
 using System;
 using System.IO;
 using System.Web.UI;
-
+using System.Collections.Generic;
 
 namespace PoshUIkit {
     public class ButtonGroup {
+        public List<string> Elements { get; set; }
         public string Html() {
             StringWriter stringWriter = new StringWriter();
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
+                writer.Indent = 1;
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-group");
                 writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
+                
+                foreach (var e in Elements) {
+                    writer.Write(e);
+                }
+                
                 writer.RenderEndTag(); // End #1
             }
             return stringWriter.ToString();
