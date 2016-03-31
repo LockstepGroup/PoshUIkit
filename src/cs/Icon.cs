39b01ece0827ec1f0bc9cfdf8b81896367b8d164
Brian Addicks <brian@addicks.us>
2016-03-31 16:54:53 -0400

    first pass at Icon.cs

---
+++ src/cs/Icon.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Web.UI;
+using System.Collections.Generic;
+
+namespace PoshUIkit {
+    public class Icon {
+        public string Type;
+        public string Name;
+        public string Size;
+        
+        // Join classes together so we can apply them at the same time
+        public string allClasses {
+            get {
+                if (String.IsNullOrEmpty(Size)) {
+                    return this.Name;
+                } else {
+                    return (this.Name + " " + this.Size);
+                }
+            }
+        }
+        
+        // Render Html
+        public string Html() {
+            StringWriter stringWriter = new StringWriter();
+            using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
+                
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, allClasses);
+                
+                if (this.Type == "span") {
+                    writer.RenderBeginTag(HtmlTextWriterTag.Span); // Begin #1
+                } else {
+                    writer.RenderBeginTag(HtmlTextWriterTag.I); // Begin #1
+                }
+                
+                writer.RenderEndTag(); // End #1
+                
+            }
+            return stringWriter.ToString();
+        }
+        
+        // Constructor
+        public Icon () {
+			this.Type = "icon";
+		}
+    }
+}
