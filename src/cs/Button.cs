e5931732eae620be9c585d62a83e1ee3df93f979
Brian Addicks <brian@addicks.us>
2016-03-31 09:31:10 -0400

    started adding dropdown to Button.cs

--- src/cs/Button.cs
+++ src/cs/Button.cs
@@ -8,6 +8,9 @@ namespace PoshUIkit {
         public List<string> Classes;
         public string Label;
         public string Link;
+        public bool IsDropdown;
+        public string DropdownMode;
+        public Dropdown DropdownContents;
         
         // Join classes together so we can apply them at the same time
         private string allClasses {
@@ -20,18 +23,34 @@ namespace PoshUIkit {
         public string Html() {
             StringWriter stringWriter = new StringWriter();
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
+                
+                // Dropdown
+                if (IsDropdown) {
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-dropdown");
+                    writer.AddAttribute("data-uk-dropdown",null);
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
+                }
+                
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, allClasses);
                 
                 if (String.IsNullOrEmpty(Link)) {
-                    writer.RenderBeginTag(HtmlTextWriterTag.Button); // Begin #1
+                    writer.RenderBeginTag(HtmlTextWriterTag.Button); // Begin #2
                 } else {
                     writer.AddAttribute(HtmlTextWriterAttribute.Href, Link);
-                    writer.RenderBeginTag(HtmlTextWriterTag.A); // Begin #1
+                    writer.RenderBeginTag(HtmlTextWriterTag.A); // Begin #2
                 }
                 
+                // Button Text
                 writer.Write(Label);
                 
-                writer.RenderEndTag(); // End #1
+                
+                writer.RenderEndTag(); // End #2
+                
+                // Dropdown
+                if (IsDropdown) {
+                    writer.RenderEndTag(); // End #1
+                }
+                
             }
             return stringWriter.ToString();
         }
