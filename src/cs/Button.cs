7f177f220c9d083e2b82822303e40237ad826fc5
Brian Addicks <brian@addicks.us>
2016-03-31 16:43:37 -0400

    reworked allClasses for Button.cs

--- src/cs/Button.cs
+++ src/cs/Button.cs
@@ -5,17 +5,20 @@ using System.Collections.Generic;
 
 namespace PoshUIkit {
     public class Button {
-        public List<string> Classes;
+        public List<string> UKClasses;
         public string Label;
         public string Link;
-        public bool IsDropdown;
-        public string DropdownMode;
-        //public Dropdown DropdownContents;
         
         // Join classes together so we can apply them at the same time
-        private string allClasses {
+        public string allClasses {
             get {
-                return string.Join(" ", Classes);
+                if (this.UKClasses.Count == 0) {
+                    return "uk-button";
+                } else {
+                    string requiredClasses = "uk-button ";
+                    requiredClasses += string.Join(" ", UKClasses); 
+                    return requiredClasses;
+                }
             }
         }
         
@@ -24,32 +27,20 @@ namespace PoshUIkit {
             StringWriter stringWriter = new StringWriter();
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
                 
-                // Dropdown
-                if (IsDropdown) {
-                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-dropdown");
-                    writer.AddAttribute("data-uk-dropdown",null);
-                    writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
-                }
-                
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, allClasses);
                 
                 if (String.IsNullOrEmpty(Link)) {
-                    writer.RenderBeginTag(HtmlTextWriterTag.Button); // Begin #2
+                    writer.RenderBeginTag(HtmlTextWriterTag.Button); // Begin #1
                 } else {
                     writer.AddAttribute(HtmlTextWriterAttribute.Href, Link);
-                    writer.RenderBeginTag(HtmlTextWriterTag.A); // Begin #2
+                    writer.RenderBeginTag(HtmlTextWriterTag.A); // Begin #1
                 }
                 
                 // Button Text
                 writer.Write(Label);
                 
                 
-                writer.RenderEndTag(); // End #2
-                
-                // Dropdown
-                if (IsDropdown) {
-                    writer.RenderEndTag(); // End #1
-                }
+                writer.RenderEndTag(); // End #1
                 
             }
             return stringWriter.ToString();
@@ -57,7 +48,7 @@ namespace PoshUIkit {
         
         // Constructor
         public Button () {
-			this.Classes = new List<string> {"uk-button"};
+			this.UKClasses = new List<string> {};
 		}
     }
 }
