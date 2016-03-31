2fefd446fe957aa1dba4e33b7b5961805a4a9028
Brian Addicks <brian@addicks.us>
2016-03-31 16:28:54 -0400

    start on Dropdown.cs

---
+++ src/cs/Dropdown.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using System.Web.UI;
+using System.Collections.Generic;
+
+namespace PoshUIkit {
+    public class Dropdown {
+        public List<string> UKClasses { get; set; }
+
+        // Join classes together so we can apply them at the same time
+        public string allClasses {
+            get {
+                if (this.UKClasses.Count == 0) {
+                    return null;
+                } else {
+                    return string.Join(" ", UKClasses);
+                }
+            }
+        }
+        
+        // Html method
+        public string Html() {
+            StringWriter stringWriter = new StringWriter();
+            using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
+                
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-dropdown");
+                writer.AddAttribute("data-uk-dropdown", null);
+                writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
+                
+                writer.RenderEndTag(); // End #1
+            }
+            return stringWriter.ToString();
+        }
+        
+        // Constructor
+        public Dropdown () {
+		}
+    }
+}
