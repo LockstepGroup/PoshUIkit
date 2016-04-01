3389c30388d47aa33f69d6476d07faa82e1ae403
Brian Addicks <brian@addicks.us>
2016-04-01 10:42:35 -0400

    start work on IsButtonGroup for Dropdown.cs

--- src/cs/Dropdown.cs
+++ src/cs/Dropdown.cs
@@ -7,6 +7,8 @@ namespace PoshUIkit {
     public class Dropdown {
         public List<string> UKClasses { get; set; }
         public Button Button;
+        public UnorderedList List;
+        public bool IsButtonGroup;
 
         // Join classes together so we can apply them at the same time
         public string allClasses {
@@ -21,16 +23,37 @@ namespace PoshUIkit {
         
         // Html method
         public string Html() {
+            // Add needed classes to List
+            List.UKClasses.Add("uk-nav");
+            List.UKClasses.Add("uk-nav-dropdown");
+            
             StringWriter stringWriter = new StringWriter();
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
                 
+                if (IsButtonGroup) {
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-group");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin Opt-1
+                }
+                
+                // Outer Div
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-dropdown");
                 writer.AddAttribute("data-uk-dropdown", null);
                 writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
                 
+                //Button
                 writer.WriteLine(Button.Html());
                 
+                //List Div
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-dropdown uk-dropdown-small");
+                writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #2
+                
+                writer.Write(List.Html());
+                
+                writer.RenderEndTag(); // End #2
                 writer.RenderEndTag(); // End #1
+                if (IsButtonGroup) {
+                    writer.RenderEndTag(); // End Opt-1
+                }
             }
             return stringWriter.ToString();
         }
