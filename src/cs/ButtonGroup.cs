053b645b381b749e87c48682041e64ad409d900c
Brian Addicks <brian@addicks.us>
2016-04-01 13:37:00 -0400

    added dropdown functionality to ButtonGroup

--- src/cs/ButtonGroup.cs
+++ src/cs/ButtonGroup.cs
@@ -5,20 +5,34 @@ using System.Collections.Generic;
 
 namespace PoshUIkit {
     public class ButtonGroup {
-        public List<string> Elements { get; set; }
+        public List<Button> Buttons;
+        public Dropdown Dropdown;
+        
         public string Html() {
             StringWriter stringWriter = new StringWriter();
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter)) {
                 writer.AddAttribute(HtmlTextWriterAttribute.Class, "uk-button-group");
                 writer.RenderBeginTag(HtmlTextWriterTag.Div); // Begin #1
                 
-                foreach (var e in Elements) {
-                    writer.Write(e);
+                if (Buttons.Count != 0) {
+                    foreach (var b in Buttons) {
+                        writer.WriteLine(b.Html());
+                    }
+                } else {
+                    writer.WriteLine(Dropdown.Button.Html());
+                    Icon caretIcon = new Icon {};
+                    caretIcon.Name = "uk-icon-caret-down";
+                    Dropdown.Button.Label = caretIcon.Html();
+                    writer.Write(Dropdown.Html());
                 }
                 
                 writer.RenderEndTag(); // End #1
             }
             return stringWriter.ToString();
         }
+        
+        public ButtonGroup () {
+			this.Buttons = new List<Button> {};
+		}
     }
 }
